78c68fb2cb750ffd0f92951192eaa4869a031007
scCoded <clark.sophie2000@gmail.com>
2022-01-14 17:25:33 +0000

    aduio
    audio

--- ai/mybot-logic.py
+++ ai/mybot-logic.py
@@ -1,30 +1,21 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Basic chatbot design --- for your own modifications - rework task A
-"""
 import aiml
 import requests
-from nltk.sem.logic import *
-from nltk.inference.resolution import *
-from nltk.sem import Expression
-from nltk.inference import ResolutionProver
-import nltk.inference.resolution as res
-from nltk.inference.nonmonotonic import *
-from nltk.inference.api import *
 from nltk import *
-from nltk.sem import logic
+import pandas as pd
+from nltk.corpus import wordnet as wn
+import string
+from nltk.inference import ResolutionProver
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from fuzzywuzzy import process
-import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from nltk.corpus import wordnet as wn
-import string
+import time
+import speech_recognition as sr
+from gtts import gTTS
+import playsound
 
 read_expr = logic.Expression.fromstring
-
 
 #######################################################
 #  Initialise Knowledgebase(s) and check the consistency
@@ -37,12 +28,19 @@ print("Please wait a moment while the integrity of the KB file checked...")
 
 [kb.append(read_expr(row)) for row in df[0]]
 
-is_valid = ResolutionProver().prove(None, kb, verbose=True)
+start = time.time()
+#measuring time of Resolution Prover vs Prover9 
+is_valid = ResolutionProver().prove(None, kb, verbose=False)
 if is_valid:
    sys.exit("The Knowledgebase is not consistent - Please remove any contradictions and run this program again.")
+"""
+if Prover9Command(assumptions=kb).prove():
+   sys.exit("The Knowledgebase is not consistent - Please remove any contradictions and run this program again.")
+"""
+end = time.time()
+print("Time taken: " + str(round(end - start, 3)) + " seconds") 
 
-
-food_groups =  ["food", "protein", "vegetable", "fruit", "carbohydrate", "dairy", "fat", "meat", "planet"]
+food_groups =  ["food", "protein", "vegetable", "fruit", "carbohydrate", "dairy", "fat", "meat"]
 synset1 = wn.synset('food.n.01')
 synset2 = wn.synset('food.n.02')
 common_foods = list(set([w for s in synset1.closure(lambda s:s.hyponyms()) for w in s.lemma_names()]))
@@ -52,7 +50,7 @@ common_foods = [item for item in common_foods if item not in alphabet_list]
 #######################################################
 #  Initialise QA Pairs
 #######################################################
-qa = pd.read_csv("sampleQA.csv").dropna()
+qa = pd.read_csv("QA.csv").dropna()
 #######################################################
 #  Initialise AIML agent
 #######################################################
@@ -79,13 +77,25 @@ print("Welcome to this chat bot. Please feel free to ask questions from me!")
 #######################################################
 # Main loop
 #######################################################
-    
+   
 def validate_expression(object, subject):
     expr = read_expr((subject + "(" + object + ")").lower())
     print(expr)
+    start = time.time()
     is_valid_expression = ResolutionProver().prove(expr, kb, verbose=False)
+    end = time.time()
+    print("Time taken: " + str(round(end - start, 3)) + " seconds") 
     return is_valid_expression
-
+"""
+def validate_expression(object, subject):
+    expr = read_expr((subject + "(" + object + ")").lower())
+    print(expr)
+    start = time.time()
+    is_valid_expression = Prover9Command(expr, assumptions=kb).prove()
+    end = time.time()
+    print("Time taken: " + str(round(end - start, 3)) + " seconds") 
+    return is_valid_expression
+"""
 def get_fuzzy_match(word, data):
     choices = []
     for key in data:
@@ -99,10 +109,41 @@ def get_fuzzy_match(word, data):
     else:
         return word
 
+#audio communication    
+r = sr.Recognizer()
+
+def get_audio():
+    with sr.Microphone() as source:
+        try:
+            # read the audio data from the default microphone
+            audio_data = r.record(source, duration=5)
+            print("Recognizing...")
+            # convert speech to text
+            text = r.recognize_google(audio_data, language="en-US")
+            return(text)
+        except:
+            return("unrecognisable")
+
+def print_with_audio(input_type, robot_input):
+    if input_type == 'text':
+        print(robot_input)
+    else:
+        print(robot_input)
+        tts = gTTS(text=robot_input, lang='en')
+        tts.save("tts.mp3")
+        playsound.playsound("tts.mp3")
+
+input_type = input("Choose 'text' or 'speech' based communication> ")
+
 while True:
     # get user input
     try:
-        user_input = input("> ")
+        if input_type == 'text':
+            user_input = input("> ")
+        else:
+            user_input = get_audio()
+            print("> " + user_input)
+            
     except (KeyboardInterrupt, EOFError) as e:
         print("Bye!")
         break
@@ -121,26 +162,26 @@ while True:
             object = get_fuzzy_match(object, common_foods)
             subject = get_fuzzy_match(subject, food_groups)
             if validate_expression(object, subject):
-                print("I already knew that")
+                print_with_audio(input_type, "I already knew that")
             else:
                 #Check reverse expression to prove if there is an rule in knowledgebase (or not) that disproved original expression
                 if validate_expression(object, ("not " + subject)):
-                    print("I can see that is not true")
+                    print_with_audio(input_type, "I can see that is NOT true")
                 else:
-                    print("OK, I will remember that", object, "is a", subject)
+                    print_with_audio(input_type, ("OK, I will remember that " + object + " is " + subject))
                     kb.append(read_expr(subject + "(" + object + ")"))
         elif cmd == 32:  # if the input pattern is "check that * is *"
             object, subject = params[1].split(" is ")
             object = get_fuzzy_match(object, common_foods)
             subject = get_fuzzy_match(subject, food_groups)
             if validate_expression(object, subject):
-                print("Correct")
+                print_with_audio(input_type, "Correct")
             else:
                 #Check reverse expression to prove if there is an rule in knowledgebase (or not) that disproved original expression
                 if validate_expression(object, ("not " + subject)):
-                    print("Incorrect")
+                    print_with_audio(input_type, "Incorrect")
                 else:
-                    print("I'm not sure...")
+                    print_with_audio(input_type, "I'm not sure...")
         elif cmd == 33: # if input pattern is "I know that * and * are a pair"
             first, second = params[1].split(" and ")
             first = get_fuzzy_match(first, common_foods)
@@ -148,16 +189,16 @@ while True:
             objects = first + "," + second
             if (validate_expression(first, "food") and validate_expression(second, "food")):
                 if validate_expression(objects, "not combo"):
-                    print("I can see that is NOT a tasty combo!")
+                    print_with_audio(input_type, "I can see that is NOT a tasty combo!")
                 else:
                     if validate_expression(objects, "pair"):
-                        print("I already know that " + params[1] + " are a classic combination.")
+                        print_with_audio(input_type, ("I already know that " + params[1] + " are a classic combination."))
                     else:
                         kb.append(read_expr("food(" + first + ") & food(" + second + ")"))
                         kb.append(read_expr("combo(" + objects + ")"))
-                        print("That new combo has been added!")
+                        print_with_audio(input_type, "That new combo has been added!")
             else:
-                print("Please use the input pattern 'I know that * is *' - To categorise these items as food before making them a pair.")
+                print_with_audio(input_type, "Please use the input pattern 'I know that * is *' - To categorise these items as food before making them a pair.")
         elif cmd == 40: # if input pattern is "how much * is in *" or "how many * are in *"
            nutrient, food = params[1].split(" is ")
            label = get_fuzzy_match(nutrient, nutrients)
@@ -167,7 +208,7 @@ while True:
            try:
                nutrient_data = response.json()["totalNutrients"][key]
                food = food.replace("_", " ").replace("-", " ")
-               print("Amount of " + label +  " in " + food + " is: " + str(round(nutrient_data["quantity"])) + nutrient_data["unit"])
+               print_with_audio(input_type, ("Amount of " + label +  " in " + food + " is: " + str(round(nutrient_data["quantity"])) + nutrient_data["unit"]))
            except KeyError:
                print("That food was not found. Check spelling.")
         elif cmd == 41: # if input pattern is "what is the full nutrition for *" or "nutrition for *"
@@ -219,10 +260,12 @@ while True:
                 try:
                     fuzzy_question = get_fuzzy_match(user_input,qa_questions)
                     i = questions.index(fuzzy_question)
-                    print("fuzzy answer: "+ answers[i-1])
-                except IndexError:
-                    print("Hey I don't understand what you just wrote to me...")
+                    print_with_audio(input_type, answers[i-1])
+                except (IndexError, ValueError) as e:
+                    print_with_audio(input_type, "Hey I don't understand what you just wrote to me...")
             else:
-                print(answers[j-1])
+                print_with_audio(input_type, answers[j-1])
+            
     else:
-        print(answer)
+        print_with_audio(input_type, answer)
+        
